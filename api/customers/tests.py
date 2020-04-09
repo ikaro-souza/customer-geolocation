6cc3d2c062b2a5f5a17cfb04e4b03a1a771b0162
Icaro <ikarosouza00@outlook.com>
2020-04-08 21:41:24 -0300

    added customer details page test

--- api/customers/tests.py
+++ api/customers/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.forms.models import model_to_dict
+from django.core.management import call_command
 from rest_framework.test import APIClient
+from rest_framework import status
 
 from .models import Customer
 
 
-CUSTOMER_LIST_URL = reverse('customers:customer-list')
+CUSTOMERS_URL = reverse('customers:customer-list')
 
 
 def create_customer(
@@ -28,20 +31,38 @@ def create_customer(
     )
 
 
+def get_customer_details_url(id):
+    return reverse('customers:customer-detail', kwargs={'pk': id})
+
+
 class CustomerTests(TestCase):
     """
     Tests the customers api endpoints
     """
 
-    def setUp(self):
-        self.client = APIClient()
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
+        call_command('populate_db')
 
     def test_fetching_customers_list(self):
         """
-        Tests if customer list is being properly fetched
+        Tests if customer list endpoint is receiving the proper data
         """
 
-        response = self.client.get(CUSTOMER_LIST_URL)
-        db_customers = Customer.objects.all()[:51]
+        response = self.client.get(CUSTOMERS_URL)
 
-        self.assertEqual(len(response.data['results']), len(db_customers))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 20)
+
+    def test_fetching_customer_details(self):
+        """
+        Tests if the customer details endpoint is receiving the proper data
+        """
+
+        response = self.client.get(get_customer_details_url(1))
+        customer = Customer.objects.get(id=1)
+        customer_fields = model_to_dict(customer).keys()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(customer_fields, response.data.keys())
