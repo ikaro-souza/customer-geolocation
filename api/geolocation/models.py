0f962cc928cd486021275ce6a2a383bc7e60826b
unknown <ikarosouza00@outlook.com>
2020-04-17 23:30:15 -0300

    created Location model

--- api/geolocation/models.py
+++ api/geolocation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from customers.models import Customer
+
+
+class Location(models.Model):
+    """
+    Contains the city and its latitude and longitude of a customer
+    """
+
+    customer_id = models.ForeignKey(
+        to=Customer, on_delete=models.CASCADE
+    )
+    city = models.CharField(max_length=100, default='')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
