e19486bfe78a987d8e74a0a83fda2bea2d03a29f
unknown <ikarosouza00@outlook.com>
2020-04-17 23:21:51 -0300

    creted geolocation app

--- api/localization/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from customers.models import Customer
-
-
-class CustomerLocalization(models.Model):
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
